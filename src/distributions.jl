e5cd9c4fb70079c60ab024518a7b3370b219dd31
Zenna Tavares <zennatavares@gmail.com>
2014-11-09 17:36:49 -0500

    var/std mixup bug

--- src/distributions.jl
+++ src/distributions.jl
@@ -12,9 +12,10 @@ random(i::Int64) = RandVarSymbolic(Float64, :(ω[$i]))
 ## Location Scale Distribution Families
 ## ====================================
 # Normal
+
 normal(i::Int64,μ::Float64,σ::Float64) =
   RandVarSymbolic(Float64,:(quantile(Normal($μ,$σ),ω[$i])))
-normal(i::Int64,μ::RV{Float64},σ::RV{Float64})= (normal(i,0.,1.) * sqrt(σ)) + μ
+normal(i::Int64,μ::RV{Float64},σ::RV{Float64})= (normal(i,0.,1.) * σ) + μ
 normal(μ,σ) = normal(genint(),μ, σ)
 
 # uniform
