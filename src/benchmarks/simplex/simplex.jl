acdf253e4eba2907dc4f6c232da13ba3ff4e6b99
Zenna Tavares <zennatavares@gmail.com>
2015-02-05 08:48:20 -0500

    64 samples

--- src/benchmarks/simplex/simplex.jl
+++ src/benchmarks/simplex/simplex.jl
@@ -73,7 +73,7 @@ function simplexbenchmark(a::Algorithm, m::RandVar, b::SimplexBenchmark)
 
   model, condition = simplex(b.ndims, m)
 
-  samples, Δt, Δb = @timed(sample(a,model,condition,3))
+  samples, Δt, Δb = @timed(sample(a,model,condition,64))
 
   @show length(samples)
   #Windows
