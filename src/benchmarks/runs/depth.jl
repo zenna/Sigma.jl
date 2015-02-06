3d6f7bc1f0d14ca242450155282ba94c52755e89
Zenna Tavares <zennatavares@gmail.com>
2015-02-06 03:27:28 -0500

    depth updates

--- src/benchmarks/runs/depth.jl
+++ src/benchmarks/runs/depth.jl
@@ -8,10 +8,10 @@ import Sigma: SigmaSMT, SigmaAI
 import Sigma: cond_sample_tlmh
 import Sigma: runbenchmarks
 
-SplitBenchmarks = [SimplexBenchmark(3,[:sample_distribution,
+SplitBenchmarks = [SimplexBenchmark(i,[:sample_distribution,
                                         :accumulative_KL,
-                                        :total_time,],100,.5)
-                   for i = [2,10]]
+                                        :total_time,],400,.1)
+                   for i = [3,10]]
 
 mh_captures = [:start_loop, :refinement_depth]
 all_splits = [weighted_partial_split, rand_partial_split]
@@ -24,14 +24,14 @@ SMTAlgorithms = [SigmaSMT(mh_captures, solver, sampler, nprocs, split)
       sampler = [cond_sample_tlmh]][:]
 
 AIAlgorithms = [SigmaAI(mh_captures, sampler, nprocs, split)
-  for nprocs = [1],
+  for nprocs = [4],
       split = all_splits,
       sampler = [cond_sample_tlmh]][:]
 
 dimbenchmarks = SplitBenchmarks
 dimalgorithms = vcat(AIAlgorithms,SMTAlgorithms)
 
-runbenchmarks(AIAlgorithms,dimbenchmarks;runname = "holesize")
+runbenchmarks(AIAlgorithms,dimbenchmarks;runname = "depth")
 
 # # function extractdata(data)
 #   xs = Int64[]
